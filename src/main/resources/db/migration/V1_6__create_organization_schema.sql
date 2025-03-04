3a9e9160818d2b36603af5a7651cbf0e7f3adb06
João Arruda <joaoarruda.info@gmail.com>
2025-03-04 00:41:31 -0300

    chore: fix jacoco config

---
+++ src/main/resources/db/migration/V1_6__create_organization_schema.sql
@@ -0,0 +1,13 @@
+-- Tabela Organization
+CREATE TABLE IF NOT EXISTS `oganization` (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      address_number VARCHAR(50),
+      address_street VARCHAR(255),
+      address_neighborhood VARCHAR(255),
+      address_zipcode VARCHAR(20),
+      address_city VARCHAR(255),
+      address_state VARCHAR(255),
+      institution_name VARCHAR(255),
+      country VARCHAR(255)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
