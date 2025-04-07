7ab341f1a480c51e60ffc08ed64e344e8272eb2e
João Arruda <40219902+joaoarruda-dev@users.noreply.github.com>
2025-04-07 20:37:45 -0300

    remove backup files

--- src/main/resources/db/migration/V1_2__insert_groups_schema.sql
+++
@@ -1,5 +0,0 @@
--- sample.groups test data
-
-INSERT INTO groups (id, name) VALUES(NEXT VALUE FOR groups_seq, 'Grupo 1 - Teste');
-INSERT INTO groups (id, name) VALUES(NEXT VALUE FOR groups_seq, 'Grupo 2 - Teste');
-INSERT INTO groups (id, name) VALUES(NEXT VALUE FOR groups_seq, 'Grupo 3 - Teste');
--- src/main/resources/db/migration/V1_7__create_marathons_schema.sql
+++
@@ -1,7 +0,0 @@
--- Tabela Marathons
-CREATE TABLE IF NOT EXISTS `marathons` (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    weight INT NOT NULL,
-    score INT NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
